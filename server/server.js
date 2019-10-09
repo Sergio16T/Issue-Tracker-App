e20bbfab03c448e0954febc2d972883fb51c78ae
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2019-10-08 19:02:30 -0500

    Added API integration with UI, saved new issue to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');  
 const {ApolloServer} = require('apollo-server-express');
 const {GraphQLScalarType} = require('graphql'); 
+const {Kind} = require('graphql/language'); 
 
 let aboutMessage = "Issue Tracker API v1.0"; 
 
@@ -34,6 +35,12 @@ const GraphQLDate = new GraphQLScalarType({
     serialize(value){
         return value.toISOString(); 
     }, 
+    parseValue(value) {
+        return new Date(value); 
+    },
+    parseLiteral(ast) {
+        return (ast.kind == Kind.STRING) ? new Date(ast.value) : undefined; 
+    },
 }); 
 const resolvers = {
     Query: {
@@ -42,6 +49,7 @@ const resolvers = {
     }, 
     Mutation:{
         setAboutMessage, 
+        issueAdd
     },
     GraphQLDate, 
 };
@@ -50,6 +58,13 @@ function setAboutMessage(_, {message}) {
 }
 function issueList() {
     return issuesDB; 
+}
+function issueAdd(_, {issue}) {
+    issue.created = new Date(); 
+    issue.id = issuesDB.length + 1; 
+    if (issue.status == undefined) issue.status = 'New'; 
+    issuesDB.push(issue);
+    return issue;
 }
 const server = new ApolloServer({
     typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
